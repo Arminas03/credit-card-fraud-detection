3f4e92ab2823575e251019e6030e3b0931f0bbab
Arminas03 <arminas.raz@gmail.com>
2024-10-02 17:26:36 +0200

    cross validation+dashboard

--- xgboost_model.py
+++ xgboost_model.py
@@ -1,17 +1,23 @@
 import optuna
 from xgboost import XGBClassifier
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import f1_score, confusion_matrix
-from optuna.study.study import Trial
+from optuna.trial import Trial
+from optuna_dashboard import run_server
 
 data = pd.read_csv("creditcard.csv")
 
 x = data.drop(columns=['Class'])
 y = data['Class']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=0)
+
+def jaccard_coef(y_pred, y_testr):
+    num_hits = sum([(y_pred[i] and y_testr[i]) for i in range(len(y_pred))])
+    num_total = sum([(y_pred[i] or y_testr[i]) for i in range(len(y_pred))])
+
+    return num_hits / num_total if num_total > 0 else 0
 
 def objective(trial: Trial):
     n_estimators = trial.suggest_int('n_estimators', 100, 500)
@@ -35,10 +41,11 @@ def objective(trial: Trial):
 
     xgb_model.fit(x_train, y_train)
 
-    y_pred = xgb_model.predict(x_test)
-    return f1_score(y_test, y_pred)
+    scores = cross_val_score(xgb_model, x_train, y_train, cv=5, scoring='f1')
+    return scores.mean()
 
-study = optuna.create_study(direction='maximize')
+storage = optuna.storages.InMemoryStorage()
+study = optuna.create_study(storage=storage, study_name='xgboost_tuning', direction='maximize')
 study.optimize(objective, n_trials=100)
 
 print(f"Best value: {study.best_value} (params: {study.best_params})")
@@ -50,3 +57,5 @@ y_pred = best_xgb_model.predict(x_test)
 
 conf_matrix = confusion_matrix(y_test, y_pred)
 print(conf_matrix)
+
+run_server(storage)
