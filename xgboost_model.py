a81174cbf82b2d1febf5c9616d0767996f4259b8
Arminas03 <arminas.raz@gmail.com>
2024-10-04 10:13:40 +0200

    style:))

--- xgboost_model.py
+++ xgboost_model.py
@@ -2,24 +2,20 @@ import optuna
 from xgboost import XGBClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import confusion_matrix
 from optuna.trial import Trial
 from optuna_dashboard import run_server
 
-data = pd.read_csv("creditcard.csv")
+def load_data(path):
+    data = pd.read_csv(path)
 
-x = data.drop(columns=['Class'])
-y = data['Class']
+    x = data.drop(columns=['Class'])
+    y = data['Class']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=0)
+    return train_test_split(x, y, train_size=0.8, random_state=0)
 
-def jaccard_coef(y_pred, y_testr):
-    num_hits = sum([(y_pred[i] and y_testr[i]) for i in range(len(y_pred))])
-    num_total = sum([(y_pred[i] or y_testr[i]) for i in range(len(y_pred))])
 
-    return num_hits / num_total if num_total > 0 else 0
-
-def objective(trial: Trial):
+def objective(trial: Trial, x_train, y_train):
     n_estimators = trial.suggest_int('n_estimators', 100, 500)
     learning_rate = trial.suggest_float('learning_rate', 0.01, 0.3)
     max_depth = trial.suggest_int('max_depth', 3, 10)
@@ -44,18 +40,27 @@ def objective(trial: Trial):
     scores = cross_val_score(xgb_model, x_train, y_train, cv=5, scoring='f1')
     return scores.mean()
 
-storage = optuna.storages.InMemoryStorage()
-study = optuna.create_study(storage=storage, study_name='xgboost_tuning', direction='maximize')
-study.optimize(objective, n_trials=100)
 
-print(f"Best value: {study.best_value} (params: {study.best_params})")
+def optimized_confusion_matrix(x_train, y_train, x_test, y_test, params):
+    best_xgb_model = XGBClassifier(**params)
+    best_xgb_model.fit(x_train, y_train)
 
-best_xgb_model = XGBClassifier(**study.best_params)
+    print(confusion_matrix(y_test, best_xgb_model.predict(x_test)))
 
-best_xgb_model.fit(x_train, y_train)
-y_pred = best_xgb_model.predict(x_test)
 
-conf_matrix = confusion_matrix(y_test, y_pred)
-print(conf_matrix)
+def main():
+    x_train, x_test, y_train, y_test = load_data("creditcard.csv")
+    n_study_trials = 100
 
-run_server(storage)
+    storage = optuna.storages.InMemoryStorage()
+    study = optuna.create_study(storage=storage, study_name='xgboost_tuning', direction='maximize')
+    study.optimize(lambda trial: objective(trial, x_train, y_train), n_trials=n_study_trials)
+
+    print(f"Best value: {study.best_value} (params: {study.best_params})")
+
+    optimized_confusion_matrix(x_train, y_train, x_test, y_test, study.best_params)
+
+    run_server(storage)
+
+if __name__ == "__main__":
+    main()
